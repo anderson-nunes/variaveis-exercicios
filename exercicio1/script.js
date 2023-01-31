e135aa8dd4e78949b895e82521a216a251719f47
anderson-nunes <andersonribeironunes@hotmail.com>
2023-01-31 09:59:16 -0300

    finalizado exercicios parte 1

---
+++ exercicio1/script.js
@@ -0,0 +1,41 @@
+//Parte 1
+
+// let nome;
+// let idade;
+
+// console.log(typeof nome, typeof idade);
+
+//uma variável sem atribuição de valor vai retornar undefined
+
+// const nome = "Anderson";
+// const idade = 37;
+
+// console.log(prompt("Qual é o seu nome?"));
+// console.log(prompt("Qual é a sua idade?"));
+
+// console.log(typeof nome, typeof idade);
+
+// //Continuou voltando como undefined.
+
+// console.log("Olá", nome, "você tem", idade);
+
+//Parte 2
+
+// let idade = prompt("Quantos anos você tem?");
+// let local = prompt("Aonde você mora?");
+// let cor = prompt("Qual a sua cor favorita?");
+
+// console.log(idade, local, cor);
+
+// idade = 38;
+// local = "Porto Alegre";
+// cor = "Amarela";
+
+// const respostaA = prompt(idade);
+// console.log(respostaA);
+
+// const respostaB = prompt(local);
+// console.log(respostaB);
+
+// const respostaC = prompt(cor);
+// console.log(respostaC);
